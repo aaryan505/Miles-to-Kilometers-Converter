f39a7060619b83e554aace410f6b8700809730e3
AARYAN GUPTA <126643804+aaryan505@users.noreply.github.com>
2024-09-02 19:14:41 +0530

    Create python miles_to_kilometers.py

---
+++ python miles_to_kilometers.py
@@ -0,0 +1,28 @@
+from tkinter import *
+def miles_to_kilo():
+    miles = float(miles_input.get())
+    km = round(miles * 1.609)
+    kilo_result_label.config(text=f"{km}")
+
+
+window = Tk()
+window.title("miles to kilo convertor")
+window.config(padx=20,pady=20)
+miles_input = Entry(width=7)
+miles_input.grid(column=1, row=0)
+
+miles_label = Label(text="Miles")
+miles_label.grid(column=2, row=0)
+
+is_equal_label = Label(text="is equal to")
+is_equal_label.grid(column=0, row=1)
+
+kilo_result_label = Label(text="0")
+kilo_result_label.grid(column=1, row=1)
+
+kilometer_label = Label(text="km")
+kilometer_label.grid(column=2, row=1)
+
+calculate_button = Button(text="calculate", command= miles_to_kilo)
+calculate_button.grid(column=1, row=2)
+window.mainloop()
